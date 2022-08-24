f4b1cbb8bd7bfd59c7b5667fe74cd5429693fa9a
Robert Lunn <robertrlunn@gmail.com>
2022-08-23 21:20:03 -0500

    starting q4

--- script/script.sql
+++ script/script.sql
@@ -32,10 +32,32 @@
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each playerâ€™s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
+-- SELECT DISTINCT(schoolname) AS name
+-- FROM schools
+-- WHERE schoolname LIKE '%Van%'
+-- OR schoolname LIKE '%_anderbilt' - looks like the data is clean...'Vanderbilt University'
 
-	
+-- SELECT p.namefirst, p.namelast, SUM(CAST(CAST(s.salary AS NUMERIC)AS MONEY)) AS total_salary, c.schoolid, p.playerid, sch.schoolname
+-- FROM people AS p
+-- LEFT JOIN salaries AS s
+-- ON s.playerid = p.playerid
+-- LEFT JOIN collegeplaying AS c
+-- ON c.playerid = s.playerid
+-- LEFT JOIN schools AS sch
+-- ON c.schoolid = sch.schoolid
+-- WHERE schoolname = 'Vanderbilt University'
+-- GROUP BY p.namefirst, p.namelast, c.schoolid, p.playerid, sch.schoolname
+-- ORDER BY total_salary DESC;
+
+-- answer - David Price $245,553,888
+
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+
+
+
+
+
    
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
    
